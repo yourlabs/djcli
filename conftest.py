bea391d775d61e8e1284a0f656cc65688dbe7f66
jpic <jpic@yourlabs.org>
2018-11-20 17:56:37 +0100

    Add tests

---
+++ conftest.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+def pytest_configure():
+    settings.configure(
+        INSTALLED_APPS=[
+            'django.contrib.contenttypes',
+            'django.contrib.auth',
+        ],
+        DATABASES=dict(default=dict(
+            ENGINE='django.db.backends.sqlite3',
+            NAME=':memory:',
+        ))
+    )
---
+++ test_djcli.py
@@ -0,0 +1,20 @@
+import sys
+
+import pytest
+
+from django.contrib.auth.models import User
+
+from djcli import ls
+
+
+@pytest.mark.django_db
+def test_ls(capsys):
+    User.objects.create(username='lol')
+    ls(
+        'settings.AUTH_USER_MODEL',
+        'username',
+        'email',
+        username='lol'
+    )
+    cap = capsys.readouterr()
+    assert 'lol' in cap.out
