94b3aa7c5bedf829beb82314eed2faed0a75f51a
jpic <jpic@yourlabs.org>
2019-01-14 04:31:26 +0100

    Upgrade to cli2 7th

--- djcli.py
+++ djcli.py
@@ -12,11 +12,30 @@ import traceback
 
 import cli2
 
-import django
 from django.apps import apps
 
 import tabulate
 
+
+@cli2.command(color=cli2.GREEN)
+def settings():
+    """Print out DJANGO_SETTINGS_MODULE."""
+    if 'DJANGO_SETTINGS_MODULE' in os.environ:
+        return os.environ['DJANGO_SETTINGS_MODULE']
+
+    found = glob.glob('**/manage.py', recursive=True)
+    if not found:
+        return
+
+    if '.' not in sys.path:
+        sys.path.insert(0, os.path.dirname(found[0]))
+
+    with open(found[0], 'r') as f:
+        for line in f.readlines():
+            m = re.match('.*[\'"]([^\'"]*.settings[^\'"]*)[\'"]', line)
+            if m:
+                mod = m.group(1)
+                return mod
 
 
 def _model_data(obj, keys=None):
@@ -47,8 +66,11 @@ def _model_get(modelname):
 def save(modelname, *args, **kwargs):
     """Update or create a model.
 
-    # Create user or update their email by username
-    djcli save auth.user username=test +email=new@email.com
+    # Create user or update their email by username, don't show their password
+    djcli save auth.user +username=test email=new@email.com -password
+
+    # same but show only email
+    djcli save auth.user +username=test +email=new@email.com email
     """
     model = _model_get(modelname)
 
@@ -63,6 +85,11 @@ def save(modelname, *args, **kwargs):
         obj = model.objects.create(**kwargs)
         created = True
 
+    if created:
+        print(f'{cli2.YELLOW}Created{cli2.RESET}')
+    else:
+        print(f'{cli2.GREEN}Updated{cli2.RESET}')
+
     print(tabulate.tabulate([
         (k, v)
         for k, v in _model_data(obj).items()
@@ -70,7 +97,7 @@ def save(modelname, *args, **kwargs):
     ]))
 
 
-@cli2.config(color=cli2.GREEN)
+@cli2.command(color=cli2.GREEN)
 def ls(modelname, *args, **kwargs):
     """Search models
 
@@ -91,7 +118,7 @@ def ls(modelname, *args, **kwargs):
     _printqs(models, args)
 
 
-@cli2.config(color=cli2.RED)
+@cli2.command(color=cli2.RED)
 def delete(modelname, *args, **kwargs):
     """
     Delete a model filtered with kwargs.
@@ -118,7 +145,7 @@ def delete(modelname, *args, **kwargs):
     print(f'Deleted {count} objects')
 
 
-@cli2.config(color=cli2.GREEN)
+@cli2.command(color=cli2.GREEN)
 def detail(modelname, *args, **kwargs):
     """Print detail for a model.
 
@@ -160,8 +187,9 @@ def chpasswd(password, **kwargs):
     print('Password updated !')
 
 
-@cli2.config(color=cli2.GREEN)
-def settings(*names):
+@cli2.command(color=cli2.GREEN)
+@cli2.option('raw', alias='r', color=cli2.GREEN, help='Raw value print')
+def setting(*names):
     """Show settings from django.
 
     How many times have you done the following ?
@@ -170,60 +198,59 @@ def settings(*names):
         from django.conf import settings
         settings.DATABASES # or something
 
-    Well it's over now ! Try this instead:
+    Well it's over now ! Try this instead to pretty print some setting:
 
-        djcli settings DATABASES INSTALLED_APPS # etc
+        djcli setting DATABASES INSTALLED_APPS # etc
 
-    To have only the value printed to stdout, use --value, ie.::
+    The --raw option will call raw print:
 
-        MEDIA_ROOT=$(djcli settings --value MEDIA_ROOT)
+        MEDIA_ROOT=$(djcli setting --value MEDIA_ROOT)
     """
-    from django.conf import settings
-
     for name in names:
-        if 'value' in console_script.parser.dashargs:
-            print(f'{getattr(settings, name)}')
+        importable = cli2.Importable.factory(f'django.conf.settings.{name}')
+        if console_script.parser.options.get('raw', False):
+            print(importable.target)
         else:
-            print(f'{name}={pprint.pformat(getattr(settings, name))}')
+            print(f'{name}={pprint.pformat(importable.target)}')
 
 
 class ConsoleScript(cli2.ConsoleScript):
-    def django_setup(self):
-        found = glob.glob('**/manage.py', recursive=True)
-        if found and 'DJANGO_SETTINGS_MODULE' not in os.environ:
-            if '.' not in sys.path:
-                sys.path.insert(0, os.path.dirname(found[0]))
+    def setup(self):
+        mod = os.getenv('DJANGO_SETTINGS_MODULE', settings())
 
-            with open(found[0], 'r') as f:
-                for line in f.readlines():
-                    m = re.match('.*[\'"]([^\'"]*.settings[^\'"]*)[\'"]', line)
-                    if m:
-                        mod = m.group(1)
-                        print(f'{cli2.GREEN}'
-                              f'Auto-detected DJANGO_SETTINGS_MODULE={mod}',
-                              file=sys.stderr)
-                        print(f'If incorrect, set DJANGO_SETTINGS_MODULE env var'
-                              f' {cli2.RESET}',
-                              file=sys.stderr)
-                        os.environ['DJANGO_SETTINGS_MODULE'] = mod
-                        break
+        if not mod:
+            raise cli2.Cli2Exception('DJANGO_SETTINGS_MODULE not found')
+
+        os.environ['DJANGO_SETTINGS_MODULE'] = mod
+
+        try:
+            import django
+        except ImportError:
+            raise cli2.Cli2Exception('ImportError: django package not found')
 
         try:
             django.setup()
         except Exception:
             print(f'{cli2.RED}Setting up django has failed !')
+
             if 'DJANGO_SETTINGS_MODULE' in os.environ:
                 print(f'DJANGO_SETTINGS_MODULE='
                       f'{os.getenv("DJANGO_SETTINGS_MODULE")}')
                 traceback.print_exc()
+
             else:
                 print('DJANGO_SETTINGS_MODULE env var not set !')
+
             print(f'{cli2.RESET}')
             sys.exit(1)
 
+        self._setup = True
+
     def call(self, command):
-        if command.name != 'help':
-            self.django_setup()
+        if command.name not in ('help', 'settings_find'):
+            if not getattr(self, '_setup', False):
+                self.setup()
+
         return super().call(command)
 
 
--- test_djcli.py
+++ test_djcli.py
@@ -4,43 +4,35 @@ Global Django test settings are located in conftest.py
 Requires pytest-django package to be installed.
 """
 import pytest
-import subprocess
-import os
-
 
 import cli2
-import djcli
 
 
 @pytest.mark.parametrize('name,command', [
-    ('setup', 'setup'),
     ('settings', 'settings'),
-    ('settings_root_urlconf', 'settings --value ROOT_URLCONF'),
+    ('setting_raw', 'setting -r DATABASES.default.NAME'),
+    ('setting', 'setting INSTALLED_APPS DATABASES.default'),
     ('ls_missing_model', 'ls'),
     ('ls_empty', 'ls auth.user username first_name'),
     ('chpasswd_empty', 'chpasswd username=fail'),
     ('delete_empty', 'delete auth.user username'),
-    ('save_staff', 'save auth.user is_staff=True username=staff -password -date_joined'),
-    ('chpasswd', 'chpasswd test username=staff'),
-    ('save_me', 'save auth.user username=me first_name=test -password -date_joined'),
-    ('ls', 'ls auth.user first_name username is_staff'),
-    ('ls_me', 'ls auth.user username=me username first_name'),
 ])
-def test_djcli(name, command):
-    if name == 'setup':
-        db = 'djcli_test_project/db.sqlite3'
-        if os.path.exists(db):
-            os.unlink(db)
-        proc = subprocess.Popen('djcli_test_project/manage.py migrate',
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        out, err = proc.communicate()
-        print(out)
-        assert not err, out + err
-    else:
-        cli2.autotest(
-            f'tests/{name}.txt',
-            'djcli ' + command,
-            [
-                '^pbkdf2_sha256[^ ]*',
-            ]
-        )
+@pytest.mark.django_db
+def test_djcli_empty(name, command):
+    cli2.autotest(f'tests/{name}.txt', 'djcli ' + command)
+
+
+story = [
+    'save auth.user is_staff=True username=staff -password -date_joined',
+    'chpasswd test username=staff',
+    'save auth.user username=me first_name=test -password -date_joined',
+    'ls auth.user first_name username is_staff',
+    'ls auth.user username=me username first_name',
+]
+
+
+@pytest.mark.django_db
+def test_djcli_crud_story():
+    """To rewrite tests: TEST_REWRITE=1 py.test"""
+    for num, cmd in enumerate(story):
+        cli2.autotest(f'tests/{num}_{cmd.split(" ")[0]}.txt', f'djcli {cmd}')
